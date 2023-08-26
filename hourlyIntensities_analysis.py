3dc3d54cf75653df4f442c183521171b145a70b0
Michael Contreras <134994892+MichaelECon95@users.noreply.github.com>
2023-08-26 00:15:21 -0500

    Update hourlyIntensities_analysis.py

--- hourlyIntensities_analysis.py
+++ hourlyIntensities_analysis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the hourlyIntensities_merged.csv data
-df_hourly_intensities = pd.read_csv('/Users/michaelcontreras/Desktop/Fitabase Data 4.12.16-5.12.16/hourlyIntensities_merged.csv')
+df_hourly_intensities = pd.read_csv('/.../hourlyIntensities_merged.csv')
 
 # Display the first few rows of the dataframe
 df_hourly_intensities.head()
